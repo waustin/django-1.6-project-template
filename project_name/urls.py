9c7862b3ffc6413bca52dd4cf0d214d6dd54da0a
Wade Austin <wade.austin@gmail.com>
2013-11-23 09:02:04 -0600

    turn on media/upload directory when DEBUG = True

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 
@@ -11,3 +12,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# UPLOAD MEDIA IN DEBUG
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
