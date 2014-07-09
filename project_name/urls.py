b391b77a37584c172c28acf4ac695076feb8902c
Wade Austin <wade.austin@gmail.com>
2014-07-09 09:21:23 -0500

    added a default home page template view

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
